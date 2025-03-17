d9b9d88f922379a2b91e8abee43b0d83e0b192cd
twdwe <denisyrkovec2011@gmail.com>
2025-03-17 10:18:54 +0300

    Add files via upload

---
+++ старый папа/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a + " - вот какое число Вы  ввели "); Console.ReadLine();
+        }
+    }
+}
